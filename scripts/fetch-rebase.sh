2e36334aee2415747f892a303a7883f41144ee71
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-06-27 17:49:34 +0200

    chore: add --tags in fetch rebase script

--- scripts/fetch-rebase.sh
+++ scripts/fetch-rebase.sh
@@ -1,5 +1,8 @@
 echo "git fetch origin master"
 git fetch origin master
 
+echo "git fetch origin master --tags"
+git fetch origin master --tags
+
 echo "git rebase origin/master"
 git rebase origin/master
